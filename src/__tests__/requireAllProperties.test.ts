82a59b439de75380e497d4e3cb0fe877e278a682
Law <law.horne@live.com>
2019-07-13 18:35:01 -0400

    Updated requireAllProperties test

---
+++ src/__tests__/requireAllProperties.test.ts
@@ -0,0 +1,49 @@
+import { requireAllProperties } from '../utils/helpers'
+
+describe('requireAllProperties', () => {
+  const schema = {
+    type: 'object',
+    properties: {
+      user: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'integer',
+          },
+          name: {
+            type: 'string',
+          },
+          email: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  }
+
+  const schemaWithReqiuiredProperties = {
+    type: 'object',
+    properties: {
+      user: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'integer',
+          },
+          name: {
+            type: 'string',
+          },
+          email: {
+            type: 'string',
+          },
+        },
+        required: ['id', 'name', 'email'],
+      },
+    },
+    required: ['user'],
+  }
+
+  it('should return a schema with all properties required', () => {
+    expect(requireAllProperties(schema)).toEqual(schemaWithReqiuiredProperties)
+  })
+})
