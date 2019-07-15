c3fb357b85edb8097547fa317a231af8cb87336d
Law <law.horne@live.com>
2019-07-15 13:10:41 -0400

    Prettier format helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,36 +1,34 @@
-import get from 'lodash.get'
+import get from 'lodash.get';
 
 export type Definition = {
-  items?: any
-  properties?: object
-  required?: string[]
-  type?: string
-}
+  items?: any;
+  properties?: object;
+  required?: string[];
+  type?: string;
+};
 
 export function requireAllProperties(definition: Definition): Definition {
-  const properties = get(definition, 'properties', false)
+  const properties = get(definition, 'properties', false);
 
   if (properties) {
-    const modifiedProperties = Object.entries(properties).map(
-      ([key, value]: [string, any]) => {
-        if (value.type === 'array') {
-          return [key, { ...value, items: requireAllProperties(value.items) }]
-        }
-        return [key, requireAllProperties(value)]
-      },
-    )
+    const modifiedProperties = Object.entries(properties).map(([key, value]: [string, any]) => {
+      if (value.type === 'array') {
+        return [key, { ...value, items: requireAllProperties(value.items) }];
+      }
+      return [key, requireAllProperties(value)];
+    });
 
     return {
       ...definition,
       required: Object.keys(properties),
       // @ts-ignore no type definitions
       properties: Object.fromEntries(modifiedProperties),
-    }
+    };
   } else if (definition.type === 'array') {
-    return { ...definition, items: requireAllProperties(definition.items) }
+    return { ...definition, items: requireAllProperties(definition.items) };
   }
 
-  return definition
+  return definition;
 }
 
 export function traverseProperties(
@@ -39,27 +37,25 @@ export function traverseProperties(
   mock: (property?: string) => string,
   modifier: (definition: Definition) => Definition,
 ): Definition {
-  const properties = get(definition, 'properties', false)
+  const properties = get(definition, 'properties', false);
 
   if (properties) {
-    const modifiedProperties = Object.entries(properties).map(
-      ([key, value]: [string, any]) => {
-        if (value.type === 'array') {
-          return [key, { ...value, items: modifier(value.items) }]
-        } else if (tester(key)) {
-          return [key, { ...value, faker: mock(key) }]
-        }
+    const modifiedProperties = Object.entries(properties).map(([key, value]: [string, any]) => {
+      if (value.type === 'array') {
+        return [key, { ...value, items: modifier(value.items) }];
+      } else if (tester(key)) {
+        return [key, { ...value, faker: mock(key) }];
+      }
 
-        return [key, modifier(value)]
-      },
-    )
+      return [key, modifier(value)];
+    });
     // @ts-ignore Object.fromEntries no type definitions
-    return { ...definition, properties: Object.fromEntries(modifiedProperties) }
+    return { ...definition, properties: Object.fromEntries(modifiedProperties) };
   } else if (definition.type === 'array') {
-    return { ...definition, items: modifier(definition.items) }
+    return { ...definition, items: modifier(definition.items) };
   }
 
-  return definition
+  return definition;
 }
 
 export function transformer(
@@ -67,11 +63,10 @@ export function transformer(
   mock: (property?: string) => string,
 ) {
   return function fn(definition: Definition): Definition {
-    return traverseProperties(definition, tester, mock, fn)
-  }
+    return traverseProperties(definition, tester, mock, fn);
+  };
 }
 
-// prettier-ignore
-export function compose<Result>(fn: (a: any) => Result,...fns: Function[]): (a: any) => Result {
-  return (...args) => fns.reduce((acc, fn) => fn(acc), fn(...args))
+export function compose<Result>(fn: (a: any) => Result, ...fns: Function[]): (a: any) => Result {
+  return (...args) => fns.reduce((acc, fn) => fn(acc), fn(...args));
 }
