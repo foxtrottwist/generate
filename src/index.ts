9d024bed04a344d3fb4c2e9554e2390aec3ea2b6
Law <law.horne@live.com>
2019-07-15 13:09:51 -0400

    Prettier format index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,38 @@
-import faker from 'faker'
-import fs from 'fs'
-import get from 'lodash.get'
+import faker from 'faker';
+import fs from 'fs';
+import get from 'lodash.get';
 // @ts-ignore no type definitions json-schema-faker
-import JSONSchemaFaker from 'json-schema-faker'
-import path from 'path'
-import SwaggerParser from 'swagger-parser'
-import 'core-js/es/object/from-entries'
-import { compose, requireAllProperties, transformer } from './utils/helpers'
-import {
-  fakeCompanyName,
-  fakeEmail,
-  fakeId,
-  fakeName,
-} from './utils/transformers'
+import JSONSchemaFaker from 'json-schema-faker';
+import path from 'path';
+import SwaggerParser from 'swagger-parser';
+import 'core-js/es/object/from-entries';
+import { compose, requireAllProperties, transformer } from './utils/helpers';
+import { fakeCompanyName, fakeEmail, fakeId, fakeName } from './utils/transformers';
 
-JSONSchemaFaker.extend('faker', () => faker)
+JSONSchemaFaker.extend('faker', () => faker);
 
-const [, , document, endpoint, dir = 'fixtures'] = process.argv
-const cwd = process.cwd()
-const fileName = endpoint.slice(0, 1).toLocaleLowerCase() + endpoint.slice(1)
-const defaultTransformers = [fakeCompanyName, fakeEmail, fakeId, fakeName]
+const [, , document, endpoint, dir = 'fixtures'] = process.argv;
+const cwd = process.cwd();
+const fileName = endpoint.slice(0, 1).toLocaleLowerCase() + endpoint.slice(1);
+const defaultTransformers = [fakeCompanyName, fakeEmail, fakeId, fakeName];
 
-async function generate(
-  api: any,
-  endpoint: string,
-  transformers = defaultTransformers,
-) {
+async function generate(api: any, endpoint: string, transformers = defaultTransformers) {
   const composedTransformers = compose(
     requireAllProperties,
     ...transformers,
-  )
+  );
 
-  const OpenDocument = await SwaggerParser.dereference(api)
-  const definition = get(OpenDocument, `definitions.${endpoint}`, {})
-  const modifiedDefinition = composedTransformers(definition)
-  const fixture = JSONSchemaFaker.generate(modifiedDefinition)
+  const OpenDocument = await SwaggerParser.dereference(api);
+  const definition = get(OpenDocument, `definitions.${endpoint}`, {});
+  const modifiedDefinition = composedTransformers(definition);
+  const fixture = JSONSchemaFaker.generate(modifiedDefinition);
 
-  !fs.existsSync(dir) ? fs.mkdirSync('fixtures') : undefined
+  !fs.existsSync(dir) ? fs.mkdirSync('fixtures') : undefined;
 
-  fs.writeFileSync(
-    path.join(cwd, `${dir}/${fileName}.json`),
-    JSON.stringify(fixture),
-  )
+  fs.writeFileSync(path.join(cwd, `${dir}/${fileName}.json`), JSON.stringify(fixture));
 }
 
-generate(document, endpoint)
+generate(document, endpoint);
 
-export default generate
-export { fakeCompanyName, fakeEmail, fakeId, transformer }
+export default generate;
+export { fakeCompanyName, fakeEmail, fakeId, fakeName, transformer };
