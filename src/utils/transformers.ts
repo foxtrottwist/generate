7bf66f990980bd00462f828588ceb24024073c26
Law <law.horne@live.com>
2019-07-15 13:11:19 -0400

    Prettier format transfomers.ts

--- src/utils/transformers.ts
+++ src/utils/transformers.ts
@@ -1,21 +1,21 @@
-import { transformer } from './helpers'
+import { transformer } from './helpers';
 
 export const fakeCompanyName = transformer(
-  property => property.includes('companyName'),
+  (property) => property.includes('companyName'),
   () => 'company.companyName',
-)
+);
 
 export const fakeEmail = transformer(
-  property => property.includes('email'),
+  (property) => property.includes('email'),
   () => 'internet.email',
-)
+);
 
 export const fakeId = transformer(
-  property => property.includes('id') || property.includes('Id'),
+  (property) => property.includes('id') || property.includes('Id'),
   () => 'random.uuid',
-)
+);
 
 export const fakeName = transformer(
-  property => property.includes('firstName') || property.includes('lastName'),
-  property => `name.${property}`,
-)
+  (property) => property.includes('firstName') || property.includes('lastName'),
+  (property) => `name.${property}`,
+);
