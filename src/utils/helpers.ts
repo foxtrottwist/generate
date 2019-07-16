934e6bb8a1e046670319e16dbbe0f1cb28d68fee
Law <law.horne@live.com>
2019-07-16 15:01:03 -0400

    Documented helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -7,6 +7,11 @@ export type Definition = {
   type?: string;
 };
 
+/**
+ * Helper function that requires all the properties in a schema definition
+ * @param {Definition} definition - The schema definition to operate on
+ * @returns {Definiton} - The schema definition with all propeties required
+ */
 export function requireAllProperties(definition: Definition): Definition {
   const properties = get(definition, 'properties', false);
 
@@ -21,7 +26,7 @@ export function requireAllProperties(definition: Definition): Definition {
     return {
       ...definition,
       required: Object.keys(properties),
-      // @ts-ignore no type definitions
+      // @ts-ignore: Object.fromEntries no type definitions
       properties: Object.fromEntries(modifiedProperties),
     };
   } else if (definition.type === 'array') {
@@ -31,6 +36,14 @@ export function requireAllProperties(definition: Definition): Definition {
   return definition;
 }
 
+/**
+ * Helper function to traverse a schema definition
+ * @param {Definition} definition - The given schema definition
+ * @param tester - The callback that will check if the property name is the property you wish to provide a mock
+ * @param mock  - The callback that will set the faker method you wish to use, after the property has been found by the tester callback
+ * @param modifier - The callback that will be called recursively to traverse the definition object
+ * @returns {Definition} - The modified schema definition
+ */
 export function traverseProperties(
   definition: Definition,
   tester: (property: string) => boolean,
@@ -49,7 +62,7 @@ export function traverseProperties(
 
       return [key, modifier(value)];
     });
-    // @ts-ignore Object.fromEntries no type definitions
+    // @ts-ignore: Object.fromEntries no type definitions
     return { ...definition, properties: Object.fromEntries(modifiedProperties) };
   } else if (definition.type === 'array') {
     return { ...definition, items: modifier(definition.items) };
@@ -58,6 +71,12 @@ export function traverseProperties(
   return definition;
 }
 
+/**
+ * Helper function to simplify the creation of other functions that traverse a schema defintion and modifies it's structure
+ * @param tester - The callback that will test the property keys to look for the given property name
+ * @param mock  - The callback that will set the faker method you wish to use, after the property has been found by the tester callback
+ * @returns {Function} - The function that will be called to operate on a schema definition
+ */
 export function transformer(
   tester: (property: string) => boolean,
   mock: (property: string) => string,
@@ -67,6 +86,12 @@ export function transformer(
   };
 }
 
+/**
+ * Helper to compose multiple functions to operate on a value; they will operate on the value in the order they were provided
+ * @param fn - The initial function to compose with at least one other, into a single function
+ * @param fns - At least one or more functions to compose into a single function
+ * @returns {Function} - The result of two or more composed functions
+ */
 export function compose<Result>(fn: (a: any) => Result, ...fns: Function[]): (a: any) => Result {
   return (...args) => fns.reduce((acc, fn) => fn(acc), fn(...args));
 }
