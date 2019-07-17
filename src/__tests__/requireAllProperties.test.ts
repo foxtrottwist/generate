08fa0f1de0c1be8bee22e5cbbb2711f26dc5df2d
Law <law.horne@live.com>
2019-07-17 14:00:03 -0400

    Added more tests for requireAllProperties

--- src/__tests__/requireAllProperties.test.ts
+++ src/__tests__/requireAllProperties.test.ts
@@ -1,49 +1,194 @@
 import { requireAllProperties } from '../utils/helpers';
 
 describe('requireAllProperties', () => {
-  const schema = {
-    type: 'object',
-    properties: {
-      user: {
-        type: 'object',
-        properties: {
-          id: {
-            type: 'integer',
-          },
-          name: {
-            type: 'string',
-          },
-          email: {
-            type: 'string',
+  it('returns a schema with all properties required', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        user: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+            },
+            name: {
+              type: 'string',
+            },
+            email: {
+              type: 'string',
+            },
           },
         },
       },
-    },
-  };
+    };
 
-  const schemaWithReqiuiredProperties = {
-    type: 'object',
-    properties: {
-      user: {
+    const schemaWithReqiuiredProperties = {
+      type: 'object',
+      properties: {
+        user: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+            },
+            name: {
+              type: 'string',
+            },
+            email: {
+              type: 'string',
+            },
+          },
+          required: ['id', 'name', 'email'],
+        },
+      },
+      required: ['user'],
+    };
+
+    expect(requireAllProperties(schema)).toEqual(schemaWithReqiuiredProperties);
+  });
+
+  it('returns a schema with all properties required when initial schema type is array', () => {
+    const schema = {
+      type: 'array',
+      items: {
         type: 'object',
         properties: {
-          id: {
-            type: 'integer',
-          },
-          name: {
-            type: 'string',
-          },
-          email: {
-            type: 'string',
+          user: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'integer',
+              },
+              name: {
+                type: 'string',
+              },
+              email: {
+                type: 'string',
+              },
+            },
           },
         },
-        required: ['id', 'name', 'email'],
       },
-    },
-    required: ['user'],
-  };
+    };
 
-  it('should return a schema with all properties required', () => {
+    const schemaWithReqiuiredProperties = {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          user: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'integer',
+              },
+              name: {
+                type: 'string',
+              },
+              email: {
+                type: 'string',
+              },
+            },
+            required: ['id', 'name', 'email'],
+          },
+        },
+        required: ['user'],
+      },
+    };
+
+    expect(requireAllProperties(schema)).toEqual(schemaWithReqiuiredProperties);
+  });
+
+  it('returns a schema with all deeply nested properties required', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        user: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+            },
+            cats: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  name: { type: 'string' },
+                  color: { type: 'string' },
+                  age: { type: 'string' },
+                },
+              },
+            },
+            owner: {
+              type: 'object',
+              properties: {
+                name: {
+                  type: 'object',
+                  properties: {
+                    firstName: {
+                      type: 'string',
+                    },
+                    lastName: {
+                      type: 'string',
+                    },
+                  },
+                },
+                sanityLevel: { type: 'integer' },
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const schemaWithReqiuiredProperties = {
+      type: 'object',
+      properties: {
+        user: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+            },
+            cats: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  name: { type: 'string' },
+                  color: { type: 'string' },
+                  age: { type: 'string' },
+                },
+                required: ['name', 'color', 'age'],
+              },
+            },
+            owner: {
+              type: 'object',
+              properties: {
+                name: {
+                  type: 'object',
+                  properties: {
+                    firstName: {
+                      type: 'string',
+                    },
+                    lastName: {
+                      type: 'string',
+                    },
+                  },
+                  required: ['firstName', 'lastName'],
+                },
+                sanityLevel: { type: 'integer' },
+              },
+              required: ['name', 'sanityLevel'],
+            },
+          },
+          required: ['id', 'cats', 'owner'],
+        },
+      },
+      required: ['user'],
+    };
+
     expect(requireAllProperties(schema)).toEqual(schemaWithReqiuiredProperties);
   });
 });
